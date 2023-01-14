1b583fb22a2e0e26161c253969f57a06373cbcb6
Mutasim Issa <mutasim@sourcya.com>
2023-01-14 03:23:29 +0200

    check for /flussonic existance

--- start-flussonic-service.sh
+++ start-flussonic-service.sh
@@ -4,5 +4,6 @@ docker run --restart unless-stopped \
     --name flussonic \
     --hostname flussonic \
     -p 5000:80 \
-    -v /flussonic/etc/flussonic:/etc/flussonic \
+    -v /flussonic/etc/flussonic:/etc/flussonic:rw \
+    -v /flussonic/var/lib/flussonic:/var/lib/flussonic:rw \
     flussonic/flussonic
