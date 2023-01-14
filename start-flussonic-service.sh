197e7324f7df6cdc6cf98f5a53cb2d08dceda160
Mutasim Issa <mutasim@sourcya.com>
2023-01-14 03:58:18 +0200

    updates

--- start-flussonic-service.sh
+++ start-flussonic-service.sh
@@ -4,7 +4,7 @@ flussonicVarDir="/flussonic/var/lib/flussonic"
 if [ -d "$flussonicEtcDir" ]; then
     echo "didn't find $flussonicEtcDir , creating default /etc/flussonic configuration ..."
     mkdir -p "$flussonicEtcDir"
-    cp -r "$(pwd)/flussonic-default-files/etc/flussonic/*" "$flussonicEtcDir"
+    rsync -avu --delete "$(pwd)/flussonic-default-files/etc/" "/flussonic/etc"
 fi
 
 if [ -d "$flussonicVarDir" ]; then
