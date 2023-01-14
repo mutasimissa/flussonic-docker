6c1c36da97b05095e542bae5dbdb3edbef89a201
Mutasim Issa <mutasim@sourcya.com>
2023-01-14 03:23:55 +0200

    check for /flussonic existance

--- start-flussonic-service.sh
+++ start-flussonic-service.sh
@@ -1,4 +1,17 @@
 #!/bin/sh
+flussonicEtcDir="/flussonic/etc/flussonic"
+flussonicVarDir="/flussonic/var/lib/flussonic"
+if [ -d "$flussonicEtcDir" ]; then
+    echo "didn't find $flussonicEtcDir , creating default /etc/flussonic configuration ..."
+    mkdir -p "$flussonicEtcDir"
+    cp -r "$(pwd)/flussonic-default-files/etc"
+fi
+
+if [ -d "$flussonicVarDir" ]; then
+    echo "didn't find $flussonicVarDir , creating required /var/lib/flussonic paths ..."
+    mkdir -p "$flussonicVarDir"
+fi
+
 docker run --restart unless-stopped \
     -d \
     --name flussonic \
