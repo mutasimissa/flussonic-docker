2b364a1ad0f8bed0466c8e0ef68f75b43570eed9
Mutasim Issa <mutasim@sourcya.com>
2023-01-14 07:55:17 +0200

    update

--- start-flussonic-container.sh
+++ start-flussonic-container.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
+httpPort=5000
+name=flussonic
 flussonicBaseDir="/flussonic-files"
 flussonicEtcDir="$flussonicBaseDir/etc/flussonic"
-flussonicVarDir="$flussonicBaseDir/var/lib/flussonic"
 dockerHubImage="flussonic/flussonic"
 if ! [ -d "$flussonicEtcDir" ]; then
     echo "didn't find $flussonicEtcDir , creating default /etc/flussonic configuration ..."
@@ -9,16 +10,15 @@ if ! [ -d "$flussonicEtcDir" ]; then
     rsync -avu --delete "$(pwd)/flussonic-default-files/etc/" "$flussonicBaseDir/etc"
 fi
 
-if ! [ -d "$flussonicVarDir" ]; then
-    echo "didn't find $flussonicVarDir , creating required /var/lib/flussonic paths ..."
-    mkdir -p "$flussonicVarDir"
+if lsof -Pi :$httpPort -sTCP:LISTEN -t >/dev/null; then
+    echo "Port in use"
+    exit 123
 fi
 
 docker run --restart unless-stopped \
     -d \
-    --name flussonic \
-    --hostname flussonic \
-    -p 5000:80 \
+    --name $name \
+    --hostname $name \
+    -p $httpPort:80 \
     -v $flussonicEtcDir:/etc/flussonic:rw \
-    -v $flussonicVarDir:/var/lib/flussonic:rw \
     $dockerHubImage
