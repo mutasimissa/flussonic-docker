da3fbc960ecd74d0324af5facdf505bb0d24a987
Mutasim Issa <mutasim@sourcya.com>
2023-01-14 03:01:25 +0200

    update

--- start-flussonic-service.sh
+++ start-flussonic-service.sh
@@ -1,10 +1,8 @@
 #!/bin/sh
-mkdir -p /flussonic/etc/flussonic &&
-    mkdir -p /flussonic/var/lib/flussonic &&
-    docker run --restart unless-stopped \
-        -d \
-        --name flussonic \
-        --hostname flussonic \
-        -p 5000:80 \
-        -v /flussonic/etc/flussonic:/etc/flussonic \
-        flussonic/flussonic
+docker run --restart unless-stopped \
+    -d \
+    --name flussonic \
+    --hostname flussonic \
+    -p 5000:80 \
+    -v /flussonic/etc/flussonic:/etc/flussonic \
+    flussonic/flussonic
